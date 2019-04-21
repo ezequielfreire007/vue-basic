58a0dcab1d49852e77951537adc7d6aa8a9120e5
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2019-04-21 18:35:14 -0300

    started project

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<template lang="pug">
+  #app
+    img(src='./assets/logo.png')
+    h1 EzeMusic
+    select(v-model="selectedCountry")
+      option(v-for="country in countries" v-bind:value="country.value") {{ country.name }} 
+    spinner(v-show="loading")
+    ul
+      artist(v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid")
+</template>
+
+<script >
+import Artist from './components/Artist.vue';
+import Spinner from './components/Spinner.vue';
+import getArtists from './api';
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      artists: [],
+      countries: [
+        { name: 'Argentina', value: 'argentina' },
+        { name: 'Colombia', value: 'colombia' },
+        { name: 'España', value: 'spain' },
+      ],
+      selectedCountry: 'argentina',
+      loading: true
+    }
+  },
+  components: {
+    Artist,
+    Spinner
+  },
+  methods: {
+    refreshArtis(){
+      const self = this
+      this.loading = true
+      getArtists(this.selectedCountry)
+        .then(function (artists) {
+          console.log(artists)
+          self.artists = artists
+          self.loading = false
+        })
+    }
+  },
+  mounted() {
+    this.refreshArtis()
+  },
+  watch: {
+    selectedCountry(){
+      this.refreshArtis()
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+  #app 
+    font-family 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    text-align center
+    color #2c3e50
+    margin-top 60px
+  img 
+    width 200px
+  h1, h2 
+    font-weight normal
+  ul 
+    list-style-type 
+    padding 0
+  li 
+    display inline-block
+    margin 0 10px
+  a 
+    color #42b983
+</style>
---
+++ src/api/config.js
@@ -0,0 +1,14 @@
+export default {
+    apiName: 'ezemusic',
+    apiKey: 'df50ffef77b6fc1847256e8e64c2016b',
+    secret: '5ba72307f2898f7e3dbbbcc39a50c1ca',
+    registeredTo: 'ezequielfreire'
+}
+
+
+/** Datos de configuracion de last.fm/api  */
+
+// Application name	ezemusic
+// API key	df50ffef77b6fc1847256e8e64c2016b
+// Shared secret	5ba72307f2898f7e3dbbbcc39a50c1ca
+// Registered to	ezequielfreire
